32c9b13e4de610a687f2c4c42843f6c8df960018
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-03-31 21:01:36 -0600

    added main files

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,24 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Vaccines from "../Vaccines";
+import Clinics from "../Clinics";
+import MedicalRecords from "../MedicalRecords";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Welcome "GENERIC NAME HERE"</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/vaccines">Vaccines</Link>
-          </li>
-          <li>
-            <Link to="/Clinics">Clinics</Link>
-          </li>
-          <li>
-            <Link to="/MedicalRecords">Medical Records</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <div>
+            <h1>Welcome "GENERIC NAME HERE"</h1>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/Vaccines">Vaccines</Link>
+                  <Route path="/Vaccines" component={Vaccines} />
+                </li>
+                <li>
+                  <Link to="/Clinics">Clinics</Link>
+                  <Route path="/Clinics" component={Clinics} />
+                </li>
+                <li>
+                  <Link to="/MedicalRecords">Medical Records</Link>
+                  <Route path="/MedicalRecords" component={MedicalRecords} />
+                </li>
+              </ul>
+            </nav>
+          </div>
+          );
+        </Switch>
+      </Router>
+    </>
   );
 };
 
